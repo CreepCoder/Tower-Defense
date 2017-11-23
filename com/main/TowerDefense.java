a1ef5119d4f6a31b0a2e2edefbca03b7240483f0
Creep Coder <21maartsander@gmail.com>
2017-11-23 12:40:16 +0100

    Game Screen

--- com/main/TowerDefense.java
+++ com/main/TowerDefense.java
@@ -1,8 +1,22 @@
 package com.main;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 public class TowerDefense {
 	TowerDefense(){
+	Display.setTitle("Hallo");
+	try {
+		Display.setDisplayMode(new DisplayMode(600, 400));
+		Display.create();
+	} catch (LWJGLException e) {
+		e.printStackTrace();
+	}
+	while (!Display.isCloseRequested()) {
+		
+	}
+	Display.destroy();
 	
 	}
 	
