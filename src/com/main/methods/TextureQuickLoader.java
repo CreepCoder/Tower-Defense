0344d429b7e8deb0bab291f48b0728e318267b69
“CreepCoder” <21maartsander@gmail.com>
2018-01-31 15:07:19 +0100

    Updated Project

---
+++ src/com/main/methods/TextureQuickLoader.java
@@ -0,0 +1,22 @@
+package com.main.methods;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
+public class TextureQuickLoader {
+	
+	public static Texture LoadTexture(String path, String filetype) {
+		Texture texture = null;
+		InputStream in = ResourceLoader.getResourceAsStream("/src/assets/tiles/" + path);
+		try {
+			texture = TextureLoader.getTexture(filetype, in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return texture;
+	}
+}
